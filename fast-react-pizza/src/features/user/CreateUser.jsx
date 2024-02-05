c057eddf327c085cf2c2df8999ccea5d5f2201b0
hatim77100 <najoua-hatim@hotmail.fr>
2024-02-05 20:54:14 +0100

    🔨 building the cart overview with redux selectors

--- fast-react-pizza/src/features/user/CreateUser.jsx
+++ fast-react-pizza/src/features/user/CreateUser.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import Button from '../../ui/Button';
+import { useNavigate } from 'react-router-dom';
 
 function CreateUser() {
   const [username, setUsername] = useState('');
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+    navigate('/menu');
   }
 
   return (
